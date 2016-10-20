1c13d23ebb88643788d30461dd5be60aeb277ded
Clint Morgan <clint.a.m@gmail.com>
2016-10-20 12:07:33 -0700

    Add simple e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,15 @@
 
 describe('QuickStart E2E Tests', function () {
 
-  let expectedMsg = 'My First Angular App';
-
-
   beforeEach(function () {
     browser.get('');
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should render the tree with sample data', function () {
+    let headingElements = element.all(by.css('h4'));
+    expect(headingElements.count()).toBe(8);
+    expect(headingElements.first().getText()).toMatch(/awareness metrics/i);
+    expect(headingElements.last().getText()).toMatch(/dimensions/i);
   });
 
 });
